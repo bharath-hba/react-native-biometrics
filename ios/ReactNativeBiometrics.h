315a555adcfd39d72c7d4fba3f78fc772508047b
bharath-hba <100030323+bharath-hba@users.noreply.github.com>
2025-01-24 17:05:17 +0530

    Update ReactNativeBiometrics.h

--- ios/ReactNativeBiometrics.h
+++ ios/ReactNativeBiometrics.h
@@ -1,9 +1,3 @@
-//
-//  ReactNativeBiometrics.h
-//
-//  Created by Brandon Hines on 4/3/18.
-//
-
 #import <React/RCTBridgeModule.h>
 
 @interface ReactNativeBiometrics : NSObject <RCTBridgeModule>
